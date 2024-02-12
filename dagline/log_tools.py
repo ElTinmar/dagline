b407750c3e172be2f4350f36c2379e6db566720b
ElTinmar <martin.privat@hotmail.fr>
2024-02-12 12:12:14 +0100

    update

--- dagline/log_tools.py
+++ dagline/log_tools.py
@@ -5,12 +5,22 @@ from typing import List
 
 def parse_logs(filename: str) -> List:
     # TODO write the proper regex
-    log_entry = re.compile('<date> <time> <process_id> <process_name> <loglevel> <num> <receive_time> <process_time> <send_time> <total_time>')
+    log_entry = re.compile('''
+        (?P<date>\d+-\d+-\d+).*
+        (?P<time>\d+:\d+:\d+,\d+).* 
+        (?P<process_id>Process-\d+).* 
+        (?P<process_name>\w+).*
+        (?P<loglevel>\w+).* 
+        (?P<num>#\d+,).*
+        (?P<receive_time>receive_time: \d+\.\d+,).* 
+        (?P<process_time>process_time: \d+\.\d+,).* 
+        (?P<send_time>send_time: \d+\.\d+,).* 
+        (?P<total_time>total_time: \d+\.\d+,)
+    ''', re.DOTALL)
     with open(filename, 'r') as f:
         content = f.read()
         entries = re.findall(log_entry, content)
     return entries
-
 
 def plot_logs(filename: str) -> None:
     
