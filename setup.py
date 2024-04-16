52cfec29ba18a293bcba64e19cc0ca86cdd538c8
ElTinmar <martin.privat@hotmail.fr>
2024-04-16 11:51:31 +0200

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='dagline',
     author='Martin Privat',
-    version='0.1.10',
+    version='0.1.11',
     packages=['dagline','dagline.tests'],
     license='MIT',
     description='DAG multiprocessing pipeline',
