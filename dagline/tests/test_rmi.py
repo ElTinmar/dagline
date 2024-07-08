8dcc9702fb0545a4327313d52fd17b54e1345533
ElTinmar <martin.privat@hotmail.fr>
2024-07-08 16:37:28 +0200

    bidirectional communication

--- dagline/tests/test_rmi.py
+++ dagline/tests/test_rmi.py
@@ -3,10 +3,11 @@ from queue import Empty
 import time
 
 class A(Process):
-    def __init__(self, stop: Event, queue, *args, **kwargs) -> None:
+    def __init__(self, stop: Event, queue_in, queue_out, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.stop = stop
-        self.queue = queue
+        self.queue_in = queue_in
+        self.queue_out = queue_out
 
     def hello(self, who, punctuation: str):
         print('hello' + who + punctuation)
@@ -14,33 +15,36 @@ class A(Process):
     def handle(self, message, args, kwargs):
         if message == 'hello':
             self.hello(*args, **kwargs)
+            self.queue_out.put('OK')
 
     def run(self):  
         while not self.stop.is_set():
             try:
-                message, arg, kwargs = self.queue.get(block=False)
+                message, arg, kwargs = self.queue_in.get(block=False)
                 self.handle(message, arg, kwargs)
             except Empty:
                 pass
             time.sleep(0.1)
         print('job finished')
 
-def rmi(queue):
+def rmi(queue_in, queue_out):
     method = 'hello'
     args = [' world']
     kwargs = {'punctuation': '!'}
     message = (method,args,kwargs)
-    queue.put(message)
+    queue_in.put(message)
+    print(queue_out.get())
 
 if __name__ == '__main__':
 
     stop = Event()
-    q = Queue()
+    q_in = Queue()
+    q_out = Queue()
 
-    a = A(stop,q)
+    a = A(stop,q_in,q_out)
     a.start()
     time.sleep(2)
-    rmi(q)
+    rmi(q_in,q_out)
     time.sleep(2)
     stop.set()
     a.join()
